9e154f2b2cfbab29038100748e5c08a9ff3a8b0f
Aghaz1-alphonic <aghaz.junaid@alphonic.in>
2021-08-09 22:08:07 +0530

    body

---
+++ src/components/Body.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import '../App.css'
+
+function Body() {
+    return (
+        <>
+                <div className="container centered">
+                    <h2 className=" text-white font-weight-bold ">
+                        Pizza Delivery
+                    </h2>
+                    <div className="text-uppercase text-white font-weight-bold weight">
+                        Maestro
+                    </div>
+                    <div className="text-uppercase text-white weight font-weight-bold piziniMargin">
+                        Pizinni
+                    </div>
+                    <p className=" text-white  about-color">
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                        A expedita rem amet sit natus quibusdam reprehenderit incidunt harum animi fuga.
+                    </p>
+                    <button type="button" className="btn btn-danger delBtn">ORDER NOW</button>
+                </div>
+        </>
+    )
+}
+
+export default Body
